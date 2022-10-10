67bce0e13aabf58e75d3d79dc46c427be75dd463
Michaela Drummonds <michaela.s.drummonds@gmail.com>
2022-10-10 16:52:01 -0400

    added address, city, state, and zip attributes

--- src/main/java/com/michaeladrummonds/aguafina/models/Customer.java
+++ src/main/java/com/michaeladrummonds/aguafina/models/Customer.java
@@ -28,14 +28,31 @@ public class Customer {
     @Column
     private String phone;
 
+    @Column
+    private String address;
+
+    @Column
+    private String city;
+
+    @Column
+    private String state;
+
+    @Column
+    private String zipCode;
+
     public Customer() {
     }
 
-    public Customer(String firstName, String lastName, String email, String phone) {
+    public Customer(String firstName, String lastName, String email, String phone, String address, String city,
+            String state, String zipCode) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.phone = phone;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
     }
 
     public Long getId() {
@@ -76,6 +93,38 @@ public class Customer {
 
     public void setPhone(String phone) {
         this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
     }
 
 }
--- src/main/java/com/michaeladrummonds/aguafina/models/Employee.java
+++ src/main/java/com/michaeladrummonds/aguafina/models/Employee.java
@@ -19,13 +19,30 @@ public class Employee {
     @Column
     private String email;
 
+    @Column
+    private String address;
+
+    @Column
+    private String city;
+
+    @Column
+    private String state;
+
+    @Column
+    private String zipCode;
+
     public Employee() {
     }
 
-    public Employee(String firstName, String lastName, String email) {
+    public Employee(String firstName, String lastName, String email, String address, String city, String state,
+            String zipCode) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
     }
 
     public Long getId() {
@@ -58,6 +75,38 @@ public class Employee {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
     }
 
 }
