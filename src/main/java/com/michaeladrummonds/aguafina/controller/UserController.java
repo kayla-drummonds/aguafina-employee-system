d495b503ad6f17d60ef7cb97f5735bd53c5a1840
Michaela Drummonds <mickeysymone@gmail.com>
2022-11-10 11:08:35 -0500

    user and admin updates

--- src/main/java/com/michaeladrummonds/aguafina/controller/UserController.java
+++ src/main/java/com/michaeladrummonds/aguafina/controller/UserController.java
@@ -1,23 +1,14 @@
 package com.michaeladrummonds.aguafina.controller;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.michaeladrummonds.aguafina.models.Role;
-import com.michaeladrummonds.aguafina.models.User;
+import com.michaeladrummonds.aguafina.models.UserRegistrationDto;
 import com.michaeladrummonds.aguafina.services.impl.UserServiceImpl;
 
 @Controller
@@ -26,34 +17,33 @@ public class UserController {
 
     private final UserServiceImpl userService;
 
-    @Autowired
-    private BCryptPasswordEncoder encoder;
-
-    public UserController(UserServiceImpl userService, BCryptPasswordEncoder encoder) {
+    public UserController(UserServiceImpl userService) {
         this.userService = userService;
-        this.encoder = encoder;
     }
 
-    @GetMapping("/registration")
+    @GetMapping("/registration/user")
     public String createNewUser(Model model) {
-        User user = new User();
+        UserRegistrationDto user = new UserRegistrationDto();
         model.addAttribute("user", user);
-        return "registration";
+        return "registration_user";
     }
 
-    @PostMapping("/registration")
-    public String registerUser(@RequestParam Map<String, String> body) {
-        User user = new User();
-        Role role = new Role();
-        role.setName("USER");
-        Collection<Role> roles = new ArrayList<>();
-        roles.add(role);
-        user.setFirstName(body.get("firstName"));
-        user.setLastName(body.get("lastName"));
-        user.setEmail(body.get("email"));
-        user.setPassword(encoder.encode(body.get("password")));
-        user.setRoles(roles);
-        userService.createNewUser(user);
+    @GetMapping("/registration/admin")
+    public String createNewAdmin(Model model) {
+        UserRegistrationDto user = new UserRegistrationDto();
+        model.addAttribute("user", user);
+        return "registration_admin";
+    }
+
+    @PostMapping("/registration/user")
+    public String registerUser(@ModelAttribute("user") UserRegistrationDto registrationDto) {
+        userService.saveUser(registrationDto);
+        return "redirect:/registration?success";
+    }
+
+    @PostMapping("/registration/admin")
+    public String registerAdmin(@ModelAttribute("user") UserRegistrationDto registrationDto) {
+        userService.saveAdmin(registrationDto);
         return "redirect:/registration?success";
     }
 
--- src/main/java/com/michaeladrummonds/aguafina/services/impl/UserServiceImpl.java
+++ src/main/java/com/michaeladrummonds/aguafina/services/impl/UserServiceImpl.java
@@ -1,12 +1,19 @@
 package com.michaeladrummonds.aguafina.services.impl;
 
+import java.util.Arrays;
+
 import javax.transaction.Transactional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import com.michaeladrummonds.aguafina.models.Role;
 import com.michaeladrummonds.aguafina.models.User;
+import com.michaeladrummonds.aguafina.models.UserRegistrationDto;
 import com.michaeladrummonds.aguafina.repository.UserRepository;
 
 @Service
@@ -14,6 +21,9 @@ import com.michaeladrummonds.aguafina.repository.UserRepository;
 public class UserServiceImpl implements UserDetailsService {
 
     private final UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
     public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
@@ -28,8 +38,23 @@ public class UserServiceImpl implements UserDetailsService {
         return user;
     }
 
-    public void createNewUser(User user) {
-        userRepository.save(user);
+    public User saveUser(UserRegistrationDto registrationDto) {
+        User user = new User();
+        user.setFirstName(registrationDto.getFirstName());
+        user.setLastName(registrationDto.getLastName());
+        user.setEmail(registrationDto.getEmail());
+        user.setPassword(passwordEncoder.encode(registrationDto.getPassword()));
+        user.setRoles(Arrays.asList(new Role("USER")));
+        return userRepository.save(user);
     }
 
+    public User saveAdmin(UserRegistrationDto registrationDto) {
+        User user = new User();
+        user.setFirstName(registrationDto.getFirstName());
+        user.setLastName(registrationDto.getLastName());
+        user.setEmail(registrationDto.getEmail());
+        user.setPassword(passwordEncoder.encode(registrationDto.getPassword()));
+        user.setRoles(Arrays.asList(new Role("USER"), new Role("ADMIN")));
+        return userRepository.save(user);
+    }
 }
