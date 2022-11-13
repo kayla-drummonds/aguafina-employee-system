e9b8a850638c4784954d38fb01a7c18744c77e92
Michaela Drummonds <mickeysymone@gmail.com>
2022-11-12 20:44:15 -0500

    removed @preauthorized, ui kit to boostrap

--- src/main/java/com/michaeladrummonds/aguafina/controller/ProductController.java
+++ src/main/java/com/michaeladrummonds/aguafina/controller/ProductController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,7 +26,6 @@ public class ProductController {
     }
 
     @GetMapping("/products")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public String getAllProducts(Model model) {
         List<Product> products = productService.getAllProducts();
         model.addAttribute("products", products);
@@ -35,7 +33,6 @@ public class ProductController {
     }
 
     @GetMapping("/products/new")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public String createNewProduct(Model model) {
         Product product = new Product();
         model.addAttribute("product", product);
@@ -43,21 +40,18 @@ public class ProductController {
     }
 
     @PostMapping("/products")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public String saveProduct(@ModelAttribute("product") Product product) {
         productService.saveProduct(product);
         return "redirect:/products";
     }
 
     @GetMapping("/products/edit/{id}")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public String editProduct(@PathVariable Integer id, Model model) {
         model.addAttribute("product", productService.getProductById(id));
         return "edit_product";
     }
 
     @PostMapping("/products/{id}")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public String updateProduct(@PathVariable Integer id, @ModelAttribute("product") Product product, Model model) {
         Product existingProduct = productService.getProductById(id);
         existingProduct.setId(product.getId());
@@ -73,7 +67,6 @@ public class ProductController {
     }
 
     @GetMapping("/products/{status}")
-    @PreAuthorize("hasRole('ADMIN')")
     public String getProductsByStatus(Model model, @Param("status") String status, Product product) {
         List<Product> productsByStatus = productService.getProductByStatus(product.getStatus());
         model.addAttribute("productsByStatus", productsByStatus);
